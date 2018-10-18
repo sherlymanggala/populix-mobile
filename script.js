f6f636930a91b3cfa4a3c009bfa17b3b1bdd5b3a
Sherly Manggala <sherly_manggala@yahoo.com>
2018-10-19 00:36:43 +0700

    add content for popover

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-$(function () {
-    $('[data-toggle="popover"]').popover({
-        trigger: 'focus'
+$(function() {
+    $('[data-toggle=popover').popover({
+        trigger: 'focus',
+        content: function() {
+            var content = $(this).attr("data-popover-content");
+            return $(content).children(".popover-body").html();
+        },
+        title: function() {
+            var title = $(this).attr("data-popover-content");
+            return $(title).children(".popover-heading").html();
+        }
     });
-  })
+});
